65150b22c215133d1babc1b61adbbb8316accb17
Alexander Jimenez <alexanderjimenez@MacBook-Pro.local>
2020-08-31 23:36:20 -0500

    Initial commit

---
+++ laravel/proyecto/resources/views/contenido/contenido.blade.php
@@ -0,0 +1,42 @@
+@extends('principal')
+@section('contenido')
+    <template v-if="menu==0">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==1">
+        <h1>holasssssssssss</h1>
+    </template>
+    <template v-if="menu==2">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==3">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==4">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==5">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==6">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==7">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==8">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==9">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==10">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==11">
+        <example-component></example-component>
+    </template>
+    <template v-if="menu==12">
+        <example-component></example-component>
+    </template>
+@endsection
